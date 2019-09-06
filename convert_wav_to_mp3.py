2bd5bf9708a1dca88dab3e010134b230edef4600
JaminJeong <mooneclipse03@gmail.com>
2019-09-06 12:06:41 +0900

    Update convert_wav_to_mp3.py
    modified error

--- convert_wav_to_mp3.py
+++ convert_wav_to_mp3.py
@@ -13,7 +13,6 @@ def make_dir(path):
 def convert_wav_to_mp3(input, output):
     assert os.path.isfile(input)
     assert input.lower().endswith('.wav')
-    assert os.path.isfile(output)
     assert output.lower().endswith('.mp3')
     AudioSegment.from_wav(input).export(output, format="mp3")
 
